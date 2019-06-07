a331ffedfb5e63ac2fdcda516df0f5dd3d6905f2
Will Streyer <wstreyer@users.noreply.github.com>
2019-06-07 14:09:26 -0500

    Updates to enhancement detection

--- test/template_match.py
+++ test/template_match.py
@@ -5,8 +5,8 @@ import numpy as np
 import os
 
 #resources
-ghclass = 'CH'
-index = 170
+ghclass = 'MT'
+index = 137
 pcwd = os.path.dirname(os.getcwd())
 imgpath = '{}\\ghclass\\{}\\img\\{}.png'.format(pcwd, ghclass, index)
 iconpath = '{}\\icons'.format(pcwd)
@@ -14,7 +14,7 @@ iconpath = '{}\\icons'.format(pcwd)
 # Specify a threshold 
 threshold = 0.8
 
-icon = 'push-2'
+icon = 'poison'
   
 # Read the main image 
 img_rgb = cv2.imread(imgpath)
@@ -23,7 +23,7 @@ img_rgb = cv2.imread(imgpath)
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
   
 # Read the template
-(x,y,w,h) = (191, 124, 25, 25)
+(x,y,w,h) = (204, 130, 28, 28)
 template = img_gray[y:y+h,x:x+w]    
   
 # Perform match operations. 
--- test/template_match_all.py
+++ test/template_match_all.py
@@ -15,7 +15,7 @@ def distance(p1: tuple, p2: tuple):
 
 #Enhancement list
 enhancements = []
-def find_enchancements(data):
+def find_enchancements(data, summon = 'none'):
     img = cv2.medianBlur(data, 5)
 
     #Detection parameters
@@ -35,16 +35,26 @@ def find_enchancements(data):
     circles = np.uint16(np.around(circles))
 
     #Find enhancement locations
-    top = [180, 80, 150, 160]
-    btm = [180, 315, 150, 150]
+    if summon == 'top':
+        (xt,yt,wt,ht) = (35, 75, 300, 165)
+        (xb,yb,wb,hb) = (180, 310, 150, 155)
+    elif summon == 'btm':
+        (xt,yt,wt,ht) = (180, 75, 150, 165)
+        (xb,yb,wb,hb) = (35, 310, 300, 155)
+    else:
+       (xt,yt,wt,ht) = (180, 75, 150, 165)
+       (xb,yb,wb,hb) = (180, 310, 150, 155)
+    
+    #Show bounding box
+    cv2.rectangle(img_rgb, (xt,yt), (xt+wt, yt+ht), (0,255,255), 2)
+    cv2.rectangle(img_rgb, (xb,yb), (xb+wb, yb+hb), (0,255,255), 2)
+    
     for circ in circles[0,:]:
         #Dot parameters
         cx = circ[0]
         cy = circ[1]
         
         #Check Top/Btm Actions
-        (xt,yt,wt,ht) = tuple(top)
-        (xb,yb,wb,hb) = tuple(btm)
         if (xt < cx < xt+wt) and (yt < cy < yt+ht):
             #cv2.circle(img_rgb,(cx,cy),3,(0,255,0),2)
             enhancements.append({'xy': (cx,cy), 'action': 'top', 'type':''})
@@ -92,7 +102,7 @@ def find_enchancements(data):
 ###Start Here##            
 #resources
 ghclass = 'MT'
-index = 137
+index = 131
 pcwd = os.path.dirname(os.getcwd())
 imgpath = '{}\\ghclass\\{}\\img\\{}.png'.format(pcwd, ghclass, index)
 iconpath = '{}\\icons'.format(pcwd)
@@ -104,9 +114,10 @@ img_rgb = cv2.imread(imgpath)
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
 
 # Specify a threshold 
-is_summon = False
-threshold0 = 0.826
-threshold1 = 0.74
+thresholds = [{'icons': ['attack', 'move', 'heal', 'shield', 'target', 'retaliate', 'push', 'pull'], 'threshold': 0.85},
+              {'icons': ['range'], 'threshold': 0.74},
+              {'icons': ['summon'], 'threshold': 0.826},
+              {'icons': ['pierce', 'poison'], 'threshold': 0.93}]
 
 #look for all known icons
 icons = []
@@ -114,10 +125,11 @@ for icon in os.listdir(iconpath):
     name = icon.split('.')[0]
     name = name.split('-')[0]
     
-    if is_summon:
-        threshold = threshold1
-    else:
-        threshold = threshold0
+    #Choose threshold from thresholds
+    for t in thresholds:
+        if name in t['icons']:
+            threshold = t['threshold']
+            break      
     
     # Read the template icon
     template = cv2.imread(os.path.join(iconpath, icon),0)
@@ -131,11 +143,11 @@ for icon in os.listdir(iconpath):
     if best[1] >= threshold:
         x = best[3][0]
         y = best[3][1]
-        print('{}: {} - {} - BEST'.format(name, best[1], (x,y)))
-        icons.append({'xy': (x,y), 'type': name})
-        #cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (0,255,0), 2)
-        if name == '0summon':
-            is_summon = True
+        #print('{}: {} - {} - BEST'.format(name, best[1], (x,y)))
+        action = 'top' if y <= 262 else 'btm'
+        icons.append({'xy': (x,y), 'size': (w,h), 'type': name, 'action': action, 'match': best[1]})
+        print(icons[-1])
+        cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (0,255,255), 2)
     
     # Find all other matches above threshold
     loc = np.where( res >= threshold)  
@@ -144,13 +156,22 @@ for icon in os.listdir(iconpath):
         x = pt[0]
         y = pt[1]
         if distance(best[3], (x,y)) > 10 and distance(prev, (x,y)) > 10:
-            print('{}: {} - {}'.format(name, res[(y,x)], (x,y)))
-            icons.append({'xy': (x,y), 'type': name})
-            #cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (0,255,255), 2)
-        prev = (x,y)
+            #print('{}: {} - {}'.format(name, res[(y,x)], (x,y)))
+            action = 'top' if y <= 262 else 'btm'
+            icons.append({'xy': (x,y), 'size': (w,h), 'type': name, 'action': action, 'match': res[(y,x)]})
+            print(icons[-1])
+            cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (0,255,255), 2)
+        prev = (x,y)        
   
 #Look for enhancements
-find_enchancements(img_gray)    
+#Check if card has a summon and which action it is (top or btm)
+summon = 'none'
+for i in icons:
+    if i['type'] == 'summon': 
+        print('Summon on {} action'.format(i['action']))
+        summon = i['action']    
+        break
+find_enchancements(img_gray, summon=summon)
 
 #Match icons with ability enhancements
 for e in enhancements:
@@ -158,12 +179,20 @@ for e in enhancements:
         for i in icons:
             dx = np.abs(e['xy'][0] - i['xy'][0])
             dy = np.abs(e['xy'][1] - i['xy'][1])
-            if dx <= 90 and dy <= 17:
-                e['type'] = i['type']
+            print('({}, {})'.format(dx, dy))
+            
+            xmax = 60 if i['action'] == summon else 90
+            if 37 <= dx <= xmax and dy <= 17:
+                if i['type'] == 'heal' and i['action'] == summon:
+                    e['type'] = 'health' if i['action'] == summon else i['type']
+                else:
+                    e['type'] = i['type']
                 print(e)
                 cv2.circle(img_rgb,e['xy'],3,(0,255,0),2)
                 x = i['xy'][0]
                 y = i['xy'][1]
+                w = i['size'][0]
+                h = i['size'][1]
                 cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (0,255,0), 2)
                 break
         else:
@@ -174,7 +203,7 @@ for e in enhancements:
 
 #Remove false enhancements
 enhancements = [e for e in enhancements if not (e['type'] == 'remove')]
-print(enhancements)
+print(len(enhancements))
 
 # Show the final image with the matched area
 cv2.imshow('Detected', img_rgb)
