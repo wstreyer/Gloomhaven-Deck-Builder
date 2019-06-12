906f72525a419dab978c3900e13027b4546cd0ad
Will Streyer <wstreyer@users.noreply.github.com>
2019-06-12 17:06:22 -0500

    Update scroll_canvas.py

--- test/scroll_canvas.py
+++ test/scroll_canvas.py
@@ -1,26 +1,41 @@
 from tkinter import *
+import os
+
+
 root=Tk()
-frame=Frame(root,width=500,height=300)
+frame=Frame(root,width=1920,height=1080)
 frame.pack()
-canvas=Canvas(frame,bg='#FFFFFF',width=500,height=300,scrollregion=(0,0,1500,300))
+canvas=Canvas(frame,bg='#FFFFFF',width=1820,height=550,scrollregion=(0,0,11200,550))
 hbar=Scrollbar(frame,orient=HORIZONTAL)
 hbar.pack(side=BOTTOM,fill=X)
 hbar.config(command=canvas.xview)
 canvas.config(xscrollcommand=hbar.set)
 inner_frame = Frame(canvas)
-canvas.create_window(600, 100, window=inner_frame)
-canvas.pack(side=LEFT, fill = BOTH, expand = TRUE)
+canvas.create_window(0, 0, anchor = NW, window=inner_frame)
+canvas.pack(side=LEFT, fill = NONE, expand = FALSE)
 
-frames = []
-labels2 = []
-labels = []
-image = PhotoImage(file='C:\\Users\\InnSight\\Documents\\Github\\Gloomhaven-Deck-Builder\\icons\\summon.png')
-for i in range(0,20):
-    frames.append(Frame(inner_frame))
-    frames[i].pack(side=LEFT)
-    labels.append(Label(frames[i], image=image, relief=SUNKEN))
-    labels[i].pack()
-    labels2.append(Label(frames[i], text = 'Frame {}'.format(i)))
-    labels2[i].pack()
+
+pcwd = os.path.dirname(os.getcwd())
+cardpath = '{}\\ghclass\\BR\\img'.format(pcwd)
+card_frames = []
+titles = []
+cards = []
+images = []
+
+class_index = 0
+num_cards = 13
+   
+for i in range(0, num_cards):    
+    images.append(PhotoImage(file = '{}\{}'.format(cardpath, '{}.png'.format(class_index+1+i))))
+    w = (images[0].width() +10)*num_cards
+    canvas.config(scrollregion=(0,0, w, 550))
+    card_frames.append(Frame(inner_frame))
+    card_frames[i].pack(side=LEFT)
+    
+    cards.append(Label(card_frames[i], image=images[i]))
+    cards[i].pack()
+    
+    titles.append(Label(card_frames[i], text = 'Card {}'.format(class_index+1+i)))
+    titles[i].pack()
 
 root.mainloop()
